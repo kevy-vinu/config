dbaceaa9db02d8a5c01313321ad911e9567d2f5f
kevy <kevy.vinu@gmail.com>
2017-05-01 14:14:27 +0530

    Script

--- Scripts/Autopoweroff.sh
+++ Scripts/Autopoweroff.sh
@@ -1,23 +1,15 @@
 loading () {
 	clear
-	echo "Auto Poweroff Scheduled for battery state 50%"
+	echo "Auto Poweroff Scheduled For Battery State 50%"
 	echo "Current Battery State $(acpi -b | cut -d" " -f4 | cut -d"," -f1)"
-	echo -ne "$1[===+]\r"
-	sleep 1s
-	echo -ne "$1[==+=]\r"
-	sleep 1s
-	echo -ne "$1[=+==]\r"
-	sleep 1s
-	echo -ne "$1[+===]\r"
-	sleep 1s
-	echo -ne "$1[+===]\r"
-	sleep 1s
-	echo -ne "$1[=+==]\r"
-	sleep 1s
-	echo -ne "$1[==+=]\r"
-	sleep 1s
-	echo -ne "$1[===+]\r"
-	sleep 1s
+	echo -ne "$1[\]\r"
+	sleep 0.3
+	echo -ne "$1[|]\r"
+	sleep 0.3
+	echo -ne "$1[/]\r"
+	sleep 0.3
+	echo -ne "$1[-]\r"
+	sleep 0.1
 }
 
 while true; do
