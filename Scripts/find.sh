08b931ab6826808c4229bc844dbdc0f5c047355e
kevy-vinu <kevy.vinu@gmail.com>
2017-08-13 23:19:38 +0530

    removed

--- Scripts/find.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
-  find /home/kevy -iname "*.pdf" | while read f;
-  do
-          if [[ $f == *.pdf ]]
-   then
-          cp -f "$f" /home/kevy/Test;
-   fi
-  done
- 
--- Scripts/hacker_like_terminal.sh
+++
@@ -1,1 +0,0 @@
-gnome-terminal -x sh -c "while [ 0 ]; do cat /var/log/udev | shuf -n 36 | grep ID | pv -q -L 21; sleep 1s; clear; done"
